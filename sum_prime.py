d136e6109c817c8fa4d040137adc3cc447fcadb6
Berwin Gan <wqg203@nyu.edu>
2018-12-28 00:33:12 +0400

    sumnation

--- sum_prime.py
+++ sum_prime.py
@@ -1,11 +1,13 @@
+import math
 #c is the highest prime in the list
 def is_prime(a,primes,c):
 	for i in primes:
+		if (i> math.sqrt(a)):
+			return 1
 		if((a%i)==0):
 			return 0
 
 	for i in range(c+2,a,2):
-		print i
 		if ((a%i)==0):
 			return 0
 	return 1
@@ -13,7 +15,7 @@ def is_prime(a,primes,c):
 total = 0
 primes =[2]
 
-for i in range(3,200,2):
+for i in range(3,2000000,2):
 	 if(is_prime(i,primes,primes[-1])==1):
 		primes.append(i)
 		total += i
