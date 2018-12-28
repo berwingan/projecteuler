c82450808901c75247a487413051dcc9d491b115
Berwin Gan <wqg203@nyu.edu>
2018-12-28 19:22:49 +0400

    champernowne

---
+++ champernowne.py
@@ -0,0 +1,15 @@
+def num_at(li,a):
+	run = ""
+	for i in range(1,a):
+		run += str(i)
+	count = 1
+	for i in li:
+		count *= int(run[i-1])
+	return count
+
+
+
+li =[1,10,100,1000,10000,100000,1000000]
+b  = num_at(li,1000000)
+
+print b
